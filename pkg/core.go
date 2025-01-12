59244098eb20450ed9e945e1a7095ea3a09ea0d9
lhdht <15229684090@163.com>
2025-01-12 11:32:45 +0800

    fix: 优化函数错误处理

--- pkg/core.go
+++ pkg/core.go
@@ -4,19 +4,13 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
 	"github.com/redis/go-redis/v9"
-	"go.uber.org/zap"
 	"os"
 	"strconv"
 	"time"
 )
 
-func Install(logger *zap.Logger, config *ConfigEntity) *redis.Client {
-	logPrefix := "install redis"
-	logger.Info(fmt.Sprintf("%s %s", logPrefix, "start ->"))
-
-	// 设置最大超时时间
+func New(config *Config) (*redis.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
@@ -37,15 +31,13 @@ func Install(logger *zap.Logger, config *ConfigEntity) *redis.Client {
 		certPool := x509.NewCertPool()
 		CAFile, CAErr := os.ReadFile(config.Tls.CaCert)
 		if CAErr != nil {
-			logger.Error(fmt.Sprintf("%s read %s error: %s", logPrefix, config.Tls.CaCert, CAErr.Error()))
-			return nil
+			return nil, CAErr
 		}
 		certPool.AppendCertsFromPEM(CAFile)
 
 		clientCert, clientCertErr := tls.LoadX509KeyPair(config.Tls.ClientCert, config.Tls.ClientCertKey)
 		if clientCertErr != nil {
-			logger.Error(fmt.Sprintf("%s tls.LoadX509KeyPair err: %v", logPrefix, clientCertErr))
-			return nil
+			return nil, clientCertErr
 		}
 
 		tlsConfig := tls.Config{
@@ -58,11 +50,8 @@ func Install(logger *zap.Logger, config *ConfigEntity) *redis.Client {
 	cli := redis.NewClient(&clientOptions)
 
 	if c := cli.Ping(ctx); c.Err() != nil {
-		logger.Error(c.Err().Error())
-		return nil
+		return nil, c.Err()
 	}
 
-	logger.Info(fmt.Sprintf("%s %s", logPrefix, "success ->"))
-
-	return cli
+	return cli, nil
 }
